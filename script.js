9ebc677bde1353208165a3ffc9bba343023fa905
Ketaki <ketakisk2018@gmail.com>
2025-08-24 01:50:01 +0530

    now playing updation

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     audioPlayer.src = songSrc;
     audioPlayer.play();
     clickSound.play();
-    nowPlayingDisplay.textContent = `Now Playing: ${songLabel}`;
+    nowPlayingDisplay.textContent = `Now Playing 🎵: ${songLabel}`;
     subtitle.style.opacity = "0";
     gramophoneDropzone.classList.add("playing");
     playBtn.style.display = "none";
